47d42477063a7300f2ca3d0c30a4933440ef15c8
lindon381 <lindonjohn@hotmail.com>
2025-05-04 18:04:41 -0400

    Create lindon381.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/lindon381.py
@@ -0,0 +1,27 @@
+"https://www.python.org"
+
+
+"Las comillas dobles sirven para hacer comentarios en una línes"
+
+'''las comillas simples repetidas tres veces sirven para
+hacer comentarios en bloque o con saltos de línea'''
+
+'''asignar variables en pythones simple, solo basta con
+poner el nombre de la variable y asignarle un valor'''
+
+x = 7
+
+''' los diferentes tipos de datos vendrían a ser:'''
+
+bool = True / False
+
+flag = 0 / 1
+
+float = 1.5
+
+int = -1 / 1
+
+
+''' para imprimir por consola solo basta con poner print () y
+dentro de los paréntesis lo que uno quiere mostrar en consola'''
+print ('hola python')
